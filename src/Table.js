58991f4b71fe36cb3c4137889523e67cb33229d8
felix <felix@volny.co>
2016-11-07 12:00:56 +0700

    purecss imported, showing a styled table

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import 'pure-css';
+
 
 
 class App extends Component {
   render() {
     return (
-      <p> Table goes here </p>
+      <table className="pure-table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+          </tr>
+      </thead>
+
+      <tbody>
+          <tr>
+            <td>1</td>
+            <td>Honda</td>
+            <td>Accord</td>
+            <td>2009</td>
+          </tr>
+        </tbody>
+      </table>
     )
   }
 }
